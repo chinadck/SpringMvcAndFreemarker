685eb278c681081998bb81da8d30a3689a940059
DongChangkun <china_dck@163.com>
2015-07-02 18:12:16 +0800

    v1.1

--- src/main/java/cn/com/fero/action/IndexAction.java
+++ src/main/java/cn/com/fero/action/IndexAction.java
@@ -1,17 +1,32 @@
 package cn.com.fero.action;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexAction {
+	private String userName;
+	
 	@RequestMapping("/index.do")
-	public String index() {
+	public String index(HttpServletRequest request, HttpServletResponse response) {
+		userName = "dongchangkun";
+		request.setAttribute("userName", userName);
 		return "index";
 	}
 	
 	@RequestMapping("/byebye.do")
 	public String byebye(){
 		return "byebye";
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
 }
